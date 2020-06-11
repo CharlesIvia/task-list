4b294bae07f3008f8e1c9a692620fd982992cd88
Charles Ivia <iviacharles@gmail.com>
2020-06-11 13:18:42 +0300

    Added event listeners

--- script.js
+++ script.js
@@ -2,55 +2,62 @@
 let todoItems = document.getElementById("todo-items"),
   todoInput = document.getElementById("todo-input"),
   todoForm = document.getElementById("todo-form"),
-  todoSearch = document.getElementById("todo-search"),
+  todoSearch = document.getElementById("todo-search");
+
+//Add event listerners
+
+todoItems.addEventListener("click", removeItem);
+todoForm.addEventListener("submit", addTodo);
+todoSearch.addEventListener("keyup", searchItem);
+document.addEventListener("DOMContentLoaded", loadItems);
 
 //Functions
 
 //Remove item
 
 function removeItem(e) {
-    if (e.target.classList.contains("btn")) {
-        let item = e.target.parentElement;
+  if (e.target.classList.contains("btn")) {
+    let item = e.target.parentElement;
 
-        //remove item from local storage
+    //remove item from local storage
 
-        removeFromLocalStorage(item);
+    removeFromLocalStorage(item);
 
-        //remove from dom
+    //remove from dom
 
-        item.remove();
-    }
+    item.remove();
+  }
 }
 
 //Add todo item
 
 function addTodo(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let todo = todoInput.value;
+  let todo = todoInput.value;
 
-    if (todo != "") {
-        //Create a list item
+  if (todo != "") {
+    //Create a list item
 
-        let li = document.createElement("li");
-        li.className = "list-group-item"
-        li.textContent = todo
+    let li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = todo;
 
-        //Create close(X) button and append it to "li"
+    //Create close(X) button and append it to "li"
 
-        let xBtn = document.createElement("button")
-        xBtn.className = "btn btn-dark btn-sm float-right"
-        xBtn.textContent = "X"
-        li.appendChild(xBtn)
+    let xBtn = document.createElement("button");
+    xBtn.className = "btn btn-dark btn-sm float-right";
+    xBtn.textContent = "X";
+    li.appendChild(xBtn);
 
-        //Append todo  item to list
+    //Append todo  item to list
 
-        todoItems.appendChild(li)
-        todoForm.reset()
+    todoItems.appendChild(li);
+    todoForm.reset();
 
-        //append todo item to local storage
+    //append todo item to local storage
 
-        addToLocalStorage(todo);
-        todoForm.reset();
-    }
+    addToLocalStorage(todo);
+    todoForm.reset();
+  }
 }
