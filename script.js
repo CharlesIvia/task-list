56720ceea2eb7a339b712c3027af8a51be721389
Charles Ivia <iviacharles@gmail.com>
2020-06-11 13:01:55 +0300

    Created logic to remove item

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ let todoItems = document.getElementById("todo-items"),
   todoInput = document.getElementById("todo-input"),
   todoForm = document.getElementById("todo-form"),
   todoSearch = document.getElementById("todo-search"),
+
+//Functions
+
+//Remove item
+
+function removeItem(e) {
+    if (e.target.classList.contains("btn")) {
+        let item = e.target.parentElement;
+
+        //remove item from local storage
+
+        removeFromLocalStorage(item);
+
+        //remove from dom
+
+        item.remove();
+    }
+}
+
