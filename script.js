5aa3651f8decfe1cf4b4ee9e8d350a0fc1aada14
Charles Ivia <iviacharles@gmail.com>
2020-06-11 12:56:20 +0300

    Fetched dom elements and set variables

---
+++ script.js
@@ -0,0 +1,5 @@
+//Fetch dom elements and set variables
+let todoItems = document.getElementById("todo-items"),
+  todoInput = document.getElementById("todo-input"),
+  todoForm = document.getElementById("todo-form"),
+  todoSearch = document.getElementById("todo-search"),
